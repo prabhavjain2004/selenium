c9d1b3c72b91ff5ade0a0a18b278d96fe44473d3
Prabhav Jain <aadimate2004@gmail.com>
2024-10-25 23:16:07 +0530

    first commit

---
+++ selenium_test.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+# Create ChromeOptions instance
+options = Options()
+
+# Initialize WebDriver with Service and Options
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+#Open Google
+driver.get("https://www.google.com")
+
+#Find the search box using its name attribute
+search_box = driver.find_element("name","q")
+
+#Type a search query
+search_box.send_keys("Selenium testing in Windows")
+
+#Simulate pressing the Enter key
+search_box.send_keys(Keys.RETURN)
+
+#Close the browser
+driver.quit()
